6ae04f12a7aba9ad93bcec7e317816791b2ed63c
sarthakPabbly <sarthak.tiwari@pabbly.com>
2025-10-28 21:05:31 +0530

    updated prisma.ts for deployment issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+  log: ['warn', 'error'],
+})
 
-const client = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV === "development") global.prisma = client;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
 
-
-export default client;
+export default prisma
